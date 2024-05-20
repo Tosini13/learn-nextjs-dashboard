d3e8495e242e7460390aceb8ee23c543cffa5b5f
Tosini13 <jbartos13@gmail.com>
2024-05-20 22:25:06 +0200

    first 5 chapters of learning next.js

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+
+const Customers:FC = () => {
+return (<p>Customers Page</p>)
+}
+
+export default Customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+
+const Invoices:FC = () => {
+return (<p>Invoices Page</p>)
+}
+
+export default Invoices;
