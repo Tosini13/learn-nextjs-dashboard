8eaaf13ea9dd5da9ab9beaee5a8b611d13b7eded
Tosini13 <jbartos13@gmail.com>
2024-05-23 14:32:57 +0200

    chapter 16 (meta) of learning next.js

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
 import { FC } from "react";
 
+export const metadata: Metadata = {
+    title: 'Customers',
+  };
+  
 
 const Customers:FC = () => {
 return (<p>Customers Page</p>)
